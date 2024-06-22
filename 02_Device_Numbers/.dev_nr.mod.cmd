93d22156ef5add5c97256697a6a3f130de7d6149
KIMRIHYEON <dlgus8648@naver.com>
2024-06-22 11:50:29 +0000

    Device_Numbers

---
+++ 02_Device_Numbers/.dev_nr.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kim/Linux_device_driver/02_Device_Numbers/dev_nr.mod := { echo  /home/kim/Linux_device_driver/02_Device_Numbers/dev_nr.o;  echo; } > /home/kim/Linux_device_driver/02_Device_Numbers/dev_nr.mod
---
+++ 02_Device_Numbers/dev_nr.mod.c
@@ -0,0 +1,37 @@
+#include <linux/build-salt.h>
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(.gnu.linkonce.this_module) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const struct modversion_info ____versions[]
+__used __section(__versions) = {
+	{ 0xfe9bc451, "module_layout" },
+	{ 0x6bc3fbc0, "__unregister_chrdev" },
+	{ 0xa202bdab, "__register_chrdev" },
+	{ 0xc5850110, "printk" },
+	{ 0x1fdc7df2, "_mcount" },
+};
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "B003F5A5FDD132F16F6D4F6");
