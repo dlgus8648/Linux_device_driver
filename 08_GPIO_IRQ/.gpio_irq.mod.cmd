c19b1ff607fdace9866ca7f734979c807c7696d7
KIMRIHYEON <dlgus8648@naver.com>
2024-06-24 12:52:04 +0000

    06_GPIO_IRQ

---
+++ 08_GPIO_IRQ/.gpio_irq.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kim/Linux_device_driver/08_GPIO_IRQ/gpio_irq.mod := { echo  /home/kim/Linux_device_driver/08_GPIO_IRQ/gpio_irq.o;  echo; } > /home/kim/Linux_device_driver/08_GPIO_IRQ/gpio_irq.mod
---
+++ 08_GPIO_IRQ/gpio_irq.mod
@@ -0,0 +1,2 @@
+/home/kim/Linux_device_driver/08_GPIO_IRQ/gpio_irq.o
+
